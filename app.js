512f9ead14fe83b6c28069ea035570d6ac8722df
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-02-09 19:51:52 +0800

    feat: add urlSeeder & add generateShortUrl function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 require('./config/mongoose')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('This is url shortener.')
