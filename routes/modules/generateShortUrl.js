03b050868a6c2fdc0df76969bc9dbe269b2e8f6f
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-02-15 17:07:59 +0800

    feat: add invalid handlebars

--- routes/modules/generateShortUrl.js
+++ routes/modules/generateShortUrl.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const validUrl = require('valid-url')
+const URL = require('../../models/url')
 
 router.post('/', (req, res) => {
-  const originalUrl = req.body.originalUrl
-  if (!validUrl.isUri(originalUrl)) {
-    return res.status(401).json("此為無效網址，請返回上一頁重新輸入")
+  const inputLongUrl = req.body.inputUrl
+  //驗證輸入的網址是否有效
+  if (!(validUrl.isUri(inputLongUrl))) {
+    console.log('not a url')
+    return URL.find()
+      .then(() => res.render('invalid', { inputLongUrl }))
+      .catch(error => console.log(error))
   }
+  //確認輸入的網址是否已在資料庫
+  //   URL.findOne({ originalUrl: inputLongUrl })
+  //     .lean()
+  //     .then()
+  //   .catch(error => console.log(error))
 })
 
 module.exports = router
